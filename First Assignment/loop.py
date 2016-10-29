03e843d9eeb519db83982159764a044087728e9a
jwj1 <jwj10@pitt.edu>
2016-10-29 15:43:25 -0400

    Assignment commit

---
+++ First Assignment/loop.py
@@ -0,0 +1,19 @@
+avg = calc_average (n1,n2,n3,n4,n5):
+gradeL = det_grade ()
+num = get_grade ()
+input validate give gradeL
+S1= get_grade("first grade)
+
+avg = calc_avarrage (s1,s2,s3,s4,s5)
+print (average score is : first)
+
+def get_grade (manage)
+    #input
+score = int(input(enter grade))
+while score <0 or score > 100
+    #user sucks
+    print ("score is between 0 and 100")
+    score = int (input(message + "Again"))
+    #end while
+print ("you enter score =" score)
+grade = get_grade 
